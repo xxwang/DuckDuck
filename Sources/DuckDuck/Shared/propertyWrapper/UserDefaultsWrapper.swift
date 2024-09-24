0289e56923835568062c22dc64a8cf4e0e3a57eb
xxwang <78023776@qq.com>
2024-09-24 09:14:07 +0800

    no message

--- Sources/DuckDuck/Shared/propertyWrapper/UserDefaultsWrapper.swift
+++ Sources/DuckDuck/Shared/propertyWrapper/UserDefaultsWrapper.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// MARK: - UserDefaultsWrapper
 @propertyWrapper
 public struct UserDefaultsWrapper<T> {
     private let userDefaults = UserDefaults.standard
@@ -22,5 +23,24 @@ public struct UserDefaultsWrapper<T> {
         self.key = key
         self.defaultValue = defaultValue
         self.userDefaults.register(defaults: [key: defaultValue])
+    }
+}
+
+// MARK: - ObjectUserDefaultsWrapper
+@propertyWrapper
+public struct ObjectUserDefaultsWrapper<T> where T: Codable {
+    private let userDefaults = UserDefaults.standard
+    private let key: String
+    private let defaultValue: T?
+
+    public var wrappedValue: T? {
+        get { return self.userDefaults.dd_object(T.self, for: key) }
+        set { self.userDefaults.dd_set(newValue, for: key) }
+    }
+
+    public init(_ key: String, defaultValue: T?) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.userDefaults.register(defaults: [key: defaultValue ?? ""])
     }
 }
