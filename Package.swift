95097e3def4c8627ae9ba0269f06b6fd1eff172b
xxwang <78023776@qq.com>
2024-11-07 14:17:19 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DuckDuck",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -14,6 +14,9 @@ let package = Package(
             name: "DuckDuck",
             targets: ["DuckDuck"]
         ),
+    ],
+    dependencies: [
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
