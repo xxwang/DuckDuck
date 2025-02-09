2b87220761875209c9c4c4a3e00c66d3085aeff7
xxwang <78023776@qq.com>
2025-02-09 23:15:58 +0800

    no message

---
+++ DuckDuck-cpp/DuckDuck.cpp
@@ -0,0 +1,8 @@
+//
+//  DuckDuck.cpp
+//  DuckDuck
+//
+//  Created by 王哥 on 9/2/2025.
+//
+
+#include "DuckDuck.h"
---
+++ DuckDuck-cpp/DuckDuck.h
@@ -0,0 +1,8 @@
+//
+//  DuckDuck.h
+//  DuckDuck
+//
+//  Created by 王哥 on 9/2/2025.
+//
+
+#include <iostream>
