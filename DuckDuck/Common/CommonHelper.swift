537e8121cad51958968619f659051255bf5e6c91
xxwang <78023776@qq.com>
2025-02-27 00:23:38 +0800

    no message

--- DuckDuck/Common/CommonHelper.swift
+++ DuckDuck/Common/CommonHelper.swift
@@ -116,3 +116,45 @@ public extension CommonHelper {
         }
     }
 }
+
+
+    // MARK: - 关联属性
+public extension CommonHelper {
+    static func bindAttribute() {
+            //TODO: - <#内容#>
+    }
+    
+    static func retrieveAttribute() -> T? {
+            //TODO: - <#内容#>
+    }
+    
+    static func unbindAllAttributes() {
+            //TODO: - <#内容#>
+    }
+}
+
+// MARK: - 关联属性工具类
+public class AttributeBinder {
+    // 为对象设置关联属性
+    public static func bind(
+        to object: AnyObject,
+        withKey key: UnsafeRawPointer,
+        value: Any?,
+        usingPolicy policy: objc_AssociationPolicy = .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+    ) {
+        objc_setAssociatedObject(object, key, value, policy)
+    }
+
+    // 获取对象的关联属性
+    public static func retrieve<T>(
+        _ object: AnyObject,
+        forKey key: UnsafeRawPointer
+    ) -> T? {
+        return objc_getAssociatedObject(object, key) as? T
+    }
+
+    // 移除对象的所有关联属性
+    public static func unbindAllAttributes(of object: AnyObject) {
+        objc_removeAssociatedObjects(object)
+    }
+}
